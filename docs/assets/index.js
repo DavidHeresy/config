771f7947b531292b2813c33ee212c7957834d396
David Härer <david@familie-haerer.de>
2021-05-17 14:27:08 +0200

    docs/

---
+++ docs/assets/index.js
@@ -0,0 +1,42 @@
+window.onload = function() {
+    setMeta();
+    setTitle();
+    setHeader();
+    setFooter();
+}
+
+
+function setMeta() {
+    fetch ("https://davidheresy.de/assets/meta.html")
+        .then(response => response.text())
+        .then(meta => {
+            document.querySelector("head").innerHTML += meta;
+        });
+}
+
+
+function setTitle() {
+    let h1 = document.querySelector("main h1");
+    document.title = h1.innerHTML + " • davidheresy.de";
+}
+
+
+function setHeader() {
+    fetch ("https://davidheresy.de/assets/header.html")
+        .then(response => response.text())
+        .then(header => {
+            let body = document.querySelector("body");
+            body.innerHTML = header + body.innerHTML;
+        });
+}
+
+
+function setFooter() {
+    fetch ("https://davidheresy.de/assets/footer.html")
+        .then(response => response.text())
+        .then(footer => {
+            let body = document.querySelector("body");
+            body.innerHTML = body.innerHTML + footer;
+        });
+}
+
