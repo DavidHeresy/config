300d844b280824ca8e7814dbd3b2d65c33c905c2
David Härer <david@familie-haerer.de>
2021-05-22 15:34:15 +0200

    docs/lesezeichen

--- docs/lesezeichen/script.js
+++ docs/lesezeichen/script.js
@@ -11,7 +11,7 @@ function populate(data) {
     let tags = document.querySelector("#tags");
     let links = document.querySelector("#links");
     tags.innerHTML = "Tags";
-    for(i = 0; i < data.length / 4; i++) {
+    for(i = 0; i < (data.length / 4) - 1; i++) {
         links.innerHTML += `<p><a href=${data[4*i]}>${data[4*i+1]}</a></p>`;
     }
 }
