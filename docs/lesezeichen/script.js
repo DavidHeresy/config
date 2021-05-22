9189ec9243a07a9d6eba732d37a081ca44f97f08
David Härer <david@familie-haerer.de>
2021-05-22 14:19:51 +0200

    docs/lesezeichen/script.js

--- docs/lesezeichen/script.js
+++ docs/lesezeichen/script.js
@@ -1,10 +1,13 @@
 function hook() {
     fetch ("./lesezeichen.txt")
         .then(response => response.text())
-        .then(lesezeichen => {
+        .then(response => {
+            let data = response.split("\n"); 
             let tags = document.querySelector("#tags");
             let links = document.querySelector("#links");
             tags.innerHTML = "Tags";
-            links.innerHTML = lesezeichen;
+            for(i = 0; i < data.length / 4; i++) {
+                links.innerHTML += `<p><a href=${data[i]}>${data[i+1]}</a></p>`
+            }
         });
 }
